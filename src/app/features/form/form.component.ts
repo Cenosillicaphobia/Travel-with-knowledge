d5c4fee09cfb0bb5f864d72d60a7303cac60bed1
Andrea <tu@esempio.com>
2023-03-22 22:50:23 +0100

    chiamata api funzionante

--- src/app/features/form/form.component.ts
+++ src/app/features/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { JsonService } from 'src/app/services/json.service';
 import { NgForm } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form',
@@ -9,13 +11,13 @@ import { NgForm } from '@angular/forms';
 })
 export class FormComponent {
 
-  constructor ( private jsonService: JsonService ){};
+  constructor ( private jsonService: JsonService, private apiService: ApiService, private http: HttpClient ){};
 
   airports = this.jsonService.airports
 
   login( form: NgForm ){
     
-    console.log(form.value)
+    this.apiService.getFootprint(form.value.origin, form.value.destination, form.value.cabinClass, form.value.passengers);
 
   }
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
-  passengers:number = 1;
-  origin:string = '';
-  destination:string = '';
-  cabinClass:string = '';
-  currencies:string = '';
+  getFootprint(origin:string, destination:string, cabinClass:string, passengers:bigint){
 
-  
+    let data = {
+      legs: [
+        {
+          from: origin,
+          to: destination,
+          passengers: passengers,
+          class: cabinClass
+        }
+      ]
+    };
+
+    let header = {
+      Authorization: 'Bearer RJ0PSQYEYWMDJDGVV3GRVETYZXRN' 
+    }
+    
+    this.http.post('https://beta3.api.climatiq.io/travel/flights', data, { headers: header})
+    .subscribe( data => { console.log(data)});
+    
+  }
 
 }
