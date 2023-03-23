2377a6666aec27196ff4b8fd8c54ada213438b3e
Andrea <tu@esempio.com>
2023-03-23 13:46:12 +0100

    aggiunta pagina home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./features/form/form.module').then(m => m.FormModule) },
+  { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'form', loadChildren: () => import('./features/form/form.module').then(m => m.FormModule) },
   { path: 'response', loadChildren: () => import('./features/response/response.module').then(m => m.ResponseModule) }
 ];
 
